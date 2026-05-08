10049d9e0403ea89f6dfe930492734e81631e1e0
Paberu Desu <pashan1ckolaeff@yandex.ru>
2026-05-08 16:54:22 +0300

    Remove generated/content build artifacts
    This cleans up generated build outputs from the engine for previous .net 8.0 and 9.0.

--- AkiGames/AkiGames/obj/Debug/net10.0-windows/AkiGames.AssemblyInfo.cs
+++ AkiGames/AkiGames/obj/Debug/net10.0-windows/AkiGames.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AkiGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49519b2c266d191016873e1d258c925f637be0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756c77495323cb7719419dfd56a5df51875a6938")]
 [assembly: System.Reflection.AssemblyProductAttribute("AkiGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AkiGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

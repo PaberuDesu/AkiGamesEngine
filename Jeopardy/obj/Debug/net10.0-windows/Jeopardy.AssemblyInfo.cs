d752f2eb6c1f8dae60ec1aff077b3207f4add33a
Paberu Desu <pashan1ckolaeff@yandex.ru>
2026-05-20 12:39:57 +0300

    z-index example minimization

--- Jeopardy/obj/Debug/net10.0-windows/Jeopardy.AssemblyInfo.cs
+++ Jeopardy/obj/Debug/net10.0-windows/Jeopardy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jeopardy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff67121fb73ff312550fa34390c72be2691e5e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfdd10b29cc71169e9ed3d470bda43a2cc2a793")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jeopardy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jeopardy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
